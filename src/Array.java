02a449f6220d98d0a6c2c4b82900bb74f832c924
kaushal <kaushalpandey14022000@gmail.com>
2025-10-07 23:38:33 +0530

    learned about arrays

--- src/Array.java
+++ src/Array.java
@@ -6,7 +6,7 @@ public class Array {
 
         Scanner in = new Scanner(System.in);
 //        syntax
-//        datatype variable_name = new datatype[size];
+//        datatype[] variable_name = new datatype[size];
         int[] arr = new int[5];
 //        or directly
         int [] a = {12,34,56,5,3,234};
@@ -18,18 +18,68 @@ public class Array {
                                    //null is a literal not a datatype it represents absence of value
 
 //        input using loops
-        System.out.println("enter numbers for array");
+        System.out.print("enter numbers for array:");
         for (int i = 0; i < 5; i++) {
             arr[i] = in.nextInt();
         }
 
 //        printing array using foreach loop
         for(int num: arr) {
-            System.out.print(num+" ");
+            System.out.println(num+" ");
         }
 
 //        easier way of printing array
         //System.out.println(Arrays.toString(arr));
+        System.out.print("enter values for array:");
+        String[] str = new String[5];
+        for (int i = 0; i < str.length; i++) {
+            str[i] = in.next();
+        }
+//        original array
+        System.out.println(Arrays.toString(str));
+//        modified array
+        str[0] = "kp";
+        System.out.println(Arrays.toString(str));
+
+        //multidimensional array
+        //syntax:
+        //datatype[][] variable_name = new datatype[row size][column size];
+        //commonly we only define size of rows since size of column can vary as we see in "d" array
+        //each individual array have different number of element
+        int [][] c = new int[3][3];
+
+        //syntax2:
+        int [][] d = {{1,3},{2,5,6},{1,6,4,6}};
+
+        System.out.println(c.length); // the length of a 2d array is its number of rows
+
+//        to enter values in a 2d array
+        for (int row = 0; row < c.length; row++) {
+            for (int col = 0; col < c[row].length; col++) {
+                c[row][col] = in.nextInt();
+            }
+        }
+
+//        to print all the values of a 2d array
+        for (int row = 0; row < c.length; row++) {
+            for (int col = 0; col < c[row].length; col++) {
+                System.out.print(c[row][col]+", ");
+            }
+            System.out.println();
+        }
+
+//        to print values using toString method
+        for (int row = 0; row < c.length; row++) {
+            System.out.println(Arrays.toString(c[row]));
+        }
+
+//        to print values using enhanced for loop
+        for (int[] x : c) {
+            System.out.println(Arrays.toString(x));
+        }
+
+
+
 
     }
 }
